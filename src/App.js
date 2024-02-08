72e95f53d57a007b86df063eafd42bc8b400f2e8
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-08 10:49:29 -0800

    use keys to show stats

--- src/App.js
+++ src/App.js
@@ -23,6 +23,37 @@ PokemonRow.propTypes = {
   onSelect: PropTypes.func,
 };
 
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {
+        Object.keys(base).map(key => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))
+      }
+    </table>
+  </div>
+)
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp Attack": PropTypes.number.isRequired,
+    "Sp Defense": PropTypes.number.isRequired,
+    Spped: PropTypes.number.isRequired,
+  }),
+
+}
+
 function App() {
   //need to track input text
   const [filter, filterSet] = React.useState("");
@@ -55,11 +86,7 @@ function App() {
             </tbody>
           </table>
         </div>
-        {selectedItem && (
-          <div>
-            <h1>{selectedItem.name.english}</h1>
-          </div>
-        )}
+        {selectedItem && <PokemonInfo {...selectedItem} />}
       </div>
     </div>
   )
